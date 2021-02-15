522323186cf16478d7902ef69ed751568a21e4cd
이름 <awaji0829@gmail.com>
2021-02-15 18:51:57 +0900

    Add : first commit. 초기세팅 완료.

---
+++ src/Data/mainStoriesData.js
@@ -0,0 +1,50 @@
+export const Fstoriesdata = [
+  {
+    id: 1,
+    img:
+      "https://freitag.rokka.io/neo_wide_thumbnail/edef6ea31cd51980ebf7a8c39a122912a70f6941/banner8.jpg",
+    name: "THE F52 MIAMI VICE STORY",
+  },
+  {
+    id: 2,
+    img:
+      "https://freitag.rokka.io/neo_wide_thumbnail/f79caa7b9193bcf706d9cd4be5f742a0a7e16fd0/freitag-wallets-studiolikeness-web-banner.jpg",
+    name: "FROM FIBERS TO F-ABRIC",
+  },
+  {
+    id: 3,
+    img:
+      "https://freitag.rokka.io/neo_wide_thumbnail/09393bc1d89dffb5ffbedf6256fc8b5377e515b4/top-moving.jpg",
+    name: "HOW-TO : HOW-TO-MOVIE",
+  },
+  {
+    id: 4,
+    img:
+      "https://freitag.rokka.io/neo_wide_thumbnail/98542701dbe17e0bd31c7d9a73b3f41451e1bb1f/freitag-f52-miamivice-story-olivernanzig-rgb-highres-04-banner.jpg",
+    name: "DON'T HURRY, BE HAPPY!",
+  },
+  {
+    id: 5,
+    img:
+      "https://freitag.rokka.io/neo_wide_thumbnail/98542701dbe17e0bd31c7d9a73b3f41451e1bb1f/freitag-f52-miamivice-story-olivernanzig-rgb-highres-04-banner.jpg",
+    name: "HI HELLO",
+  },
+  {
+    id: 6,
+    img:
+      "https://freitag.rokka.io/neo_wide_thumbnail/98542701dbe17e0bd31c7d9a73b3f41451e1bb1f/freitag-f52-miamivice-story-olivernanzig-rgb-highres-04-banner.jpg",
+    name: "MAKE YOU DEREAM",
+  },
+  {
+    id: 7,
+    img:
+      "https://freitag.rokka.io/neo_wide_thumbnail/98542701dbe17e0bd31c7d9a73b3f41451e1bb1f/freitag-f52-miamivice-story-olivernanzig-rgb-highres-04-banner.jpg",
+    name: "I WANT TO YOU",
+  },
+  {
+    id: 8,
+    img:
+      "https://freitag.rokka.io/neo_wide_thumbnail/98542701dbe17e0bd31c7d9a73b3f41451e1bb1f/freitag-f52-miamivice-story-olivernanzig-rgb-highres-04-banner.jpg",
+    name: "MERRY CHRISTMAS!",
+  },
+];
