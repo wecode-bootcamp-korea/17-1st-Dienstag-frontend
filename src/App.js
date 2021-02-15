11323833dacf0fc02d0204efd00bf875c2afdbb9
이름 <awaji0829@gmail.com>
2021-02-15 19:43:01 +0900

    fix :불필요한 파일 정리

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Routes from "./Routes";
-import Nav from "./Components/Nav/Nav";
 import "./App.css";
-import  "./Styles/common.css";
+import "./Styles/common.css";
 
 function App() {
   return (
     <div className="App">
       <Routes />
-      <Nav/>
-      
     </div>
   );
 }
--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./Pages/Main/Main";
+import Nav from "./Components/Nav/Nav";
 
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
+        <Nav/>
         <Switch>
           <Route exact path="/" component={Main} />
         </Switch>
