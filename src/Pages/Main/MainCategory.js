e6a524521a32f905ec7faebbd4a66bc64ccac75a
이름 <awaji0829@gmail.com>
2021-02-17 00:14:10 +0900

    fix : 메인카테고리 컴포넌트 반복되는 코드 json으로 데이터화 하여 map사용

--- src/Pages/Main/MainCategory.js
+++ src/Pages/Main/MainCategory.js
@@ -1,51 +1,48 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Main.scss';
 
 class MainCategory extends Component {
+  constructor() {
+    super();
+    this.state = { mainCategoryData: [] };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/data/mainCategoryData.json', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          mainCategoryData: data,
+        });
+        console.log(data);
+      });
+  }
+
   render() {
+    const { mainCategoryData } = this.state;
     return (
       <>
         <div className="shop">SHOP</div>
         <div className="categorycontainer">
-          <div className="main-categorys">
-            <div className="main-categorycontainer">
-              <img
-                alt="main-category"
-                src="https://images.unsplash.com/photo-1581605405669-fcdf81165afa?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjR8fG1lc3NlbmdlciUyMGJhZ3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                className="main-categoryimg"
-              ></img>
-              <div className="main-categorytext">BACKPACK</div>
-            </div>
-          </div>
-          <div className="main-categorys">
-            <div className="main-categorycontainer">
-              <img
-                alt="main-category"
-                src="https://images.unsplash.com/photo-1458836229581-464bd8518d67?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lc3NlbmdlciUyMGJhZ3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                className="main-categoryimg"
-              ></img>
-              <div className="main-categorytext">MESSENEGER</div>
-            </div>
-          </div>
-          <div className="main-categorys">
-            <div className="main-categorycontainer">
-              <img
-                alt="main-category"
-                src="https://images.unsplash.com/photo-1601592996763-f05c9c80a7f1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FyZCUyMHdhbGxldHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                className="main-categoryimg"
-              ></img>
-              <div className="main-categorytext">ACCESSORIES</div>
-            </div>
-          </div>
-          <div className="main-categorys">
-            <div className="main-categorycontainer">
-              <img
-                alt="main-category"
-                src="https://images.unsplash.com/photo-1572196284554-4e321b0e7e0b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8YmFnfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                className="main-categoryimg"
-              ></img>
-              <div className="main-categorytext">TOTEBAG</div>
-            </div>
-          </div>
+          {mainCategoryData.map(category => {
+            return (
+              <div className="main-categorys">
+                <Link to={category.link} style={{ textDecoration: 'none' }}>
+                  <div className="main-categorycontainer">
+                    <img
+                      alt="main-category"
+                      src={category.src}
+                      className="main-categoryimg"
+                    ></img>
+                    <div className="main-categorytext">{category.name}</div>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </>
     );
