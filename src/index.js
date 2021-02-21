951330f3809601e50c6b028d6bc4c2f165fe0b8a
mia <cocoball200@gmail.com>
2021-02-21 22:02:57 +0900

    Add: cart View, nav에 위치한 카트에 대한 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ProductProvider } from './context';
 import Routes from './Routes';
 import './Styles/common.scss';
 import './Styles/reset.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ProductProvider>
     <Routes />
-  </React.StrictMode>,
+  </ProductProvider>,
   document.getElementById('root')
 );
