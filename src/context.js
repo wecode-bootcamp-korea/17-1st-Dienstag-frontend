2cbfbb618c1f8d7ae2752b16f796d5edebfa5cdb
mia <cocoball200@gmail.com>
2021-02-23 08:04:20 +0900

    장바구니 추가, 삭제,fetch 구현

--- src/context.js
+++ src/context.js
@@ -12,20 +12,35 @@ class ProductProvider extends Component {
       cartList: [],
       isNavOpen: false,
       isCartOpen: false,
+      noneUserCart: [],
+      token: localStorage.getItem('token'),
+      isFilledCart: false,
+      totalPrice: 0,
     };
   }
   componentDidMount() {
-    fetch('/data/totalProducts.json', {
+    fetch(`http://10.58.2.91:8000/cart`, {
       method: 'GET',
+      headers: { Authorization: this.state.token },
     })
       .then(res => res.json())
       .then(data => {
-        this.setState({
-          totalProducts: data[0].totalProducts,
-        });
+        const {
+          total_items_list: cartList,
+          total_price: totalPrice,
+          total_products: totalProducts,
+        } = data.data;
+        this.setState(
+          {
+            cartList: cartList,
+            totalPrice: totalPrice,
+            totalProducts: totalProducts,
+          },
+          () => this.state.cartList
+        );
       });
   }
-
+  // nav
   openNav = () => {
     this.setState({
       isNavOpen: !this.state.isNavOpen,
@@ -38,6 +53,59 @@ class ProductProvider extends Component {
       isNavOpen: this.state.isNavOpen && !this.state.isNavOpen,
     });
   };
+
+  //cart
+  //장바구니 추가
+  handleCart = (bagId, token) => {
+    fetch('http://10.58.2.91:8000/cart', {
+      method: 'POST',
+      headers: { Authorization: token },
+      body: JSON.stringify({
+        product_id: 4,
+      }),
+    })
+      .then(response => response.json())
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(result => {
+        this.handleCartLsit();
+      });
+  };
+
+  handleCartLsit = () => {
+    const token = this.getToken();
+    fetch(`http://10.58.2.91:8000/cart`, {
+      method: 'GET',
+      headers: { Authorization: token },
+    })
+      .then(res => res.json())
+      .then(data => {
+        const {
+          total_items_list: cartList,
+          total_price: totalPrice,
+          total_products: totalProducts,
+        } = data.data;
+        this.setState({
+          cartList: cartList,
+          totalPrice: totalPrice,
+          totalProducts: totalProducts,
+        });
+      });
+  };
+
+  deleteCart = id => {
+    const token = localStorage.getItem('token');
+    fetch(`http://10.58.2.91:8000/cart/${4}`, {
+      method: 'DELETE',
+      headers: { Authorization: token },
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.handleCartLsit();
+      });
+  };
+
   render() {
     return (
       <ProductContext.Provider
@@ -45,6 +113,9 @@ class ProductProvider extends Component {
           ...this.state,
           openNav: this.openNav,
           showCart: this.showCart,
+          handleCart: this.handleCart,
+          handleCartLsit: this.handleCartLsit,
+          deleteCart: this.deleteCart,
         }}
       >
         {this.props.children}
